f6bee0644cef7cff2c2ea508e9fdb793f4be7697
kantoniak <kantoniak@kantoniak.com>
2026-03-08 21:06:30 -0700

    Convert blink sketch to fade in/out using PWM timer

--- firmware/src/main.cpp
+++ firmware/src/main.cpp
@@ -6,14 +6,44 @@
 int main()
 {
   SystemInit();
+  Delay_Ms(100);
 
-  funGpioInitC();
-  funPinMode(PIN_LED, GPIO_Speed_10MHz | GPIO_CNF_OUT_PP);
+  // Enable GPIO C and TIM1
+  RCC->APB2PCENR |= RCC_APB2Periph_GPIOC | RCC_APB2Periph_TIM1;
 
+  // Set PC4 to alternate function output for TIM1_CH4
+  funPinMode(PC4, GPIO_Speed_10MHz | GPIO_CNF_OUT_PP_AF);
+
+  // Reset TIM1
+  RCC->APB2PRSTR |= RCC_APB2Periph_TIM1;
+  RCC->APB2PRSTR &= ~RCC_APB2Periph_TIM1;
+
+  // Set values, enable preload, emit change event
+  // TODO: Review how to compute these values for different frequencies and duty cycles.
+  TIM1->PSC = 0;
+  TIM1->ATRLR = 2048 - 1;
+  TIM1->CTLR1 = TIM_ARPE;
+  TIM1->SWEVGR |= TIM_UG;
+
+  // Enable TIM1_CH4
+  TIM1->CCER |= TIM_CC4E | TIM_CC4P; // Active low
+  TIM1->CHCTLR2 |= TIM_OC4M_2 | TIM_OC4M_1 | TIM_OC4PE;
+
+  TIM1->CH4CVR = 0;
+  TIM1->BDTR |= TIM_MOE;
+  TIM1->CTLR1 |= TIM_CEN;
+
+  // We're moving within [1, arr-1] to avoid the weird behavior of 0 and arr.
+  uint32_t arr = (TIM1->ATRLR + 1) / 8;
+  uint32_t count = 1;
+  bool up = true;
   while(true) {
-    funDigitalWrite(PIN_LED, FUN_HIGH);
-    Delay_Ms(400);
-    funDigitalWrite(PIN_LED, FUN_LOW);
-    Delay_Ms(400);
+    TIM1->CH4CVR = count;
+    if (up) {
+      if (++count >= (arr-1)) up = false;
+    } else {
+      if (count-- <= 1) up = true;
+    }
+    Delay_Ms(5);
   }
 }
